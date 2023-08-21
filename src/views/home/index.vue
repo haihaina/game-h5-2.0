0588513a9da5244142bbf21fbaae6addcb6fcc2b
17602334124 <2415172072@qq.com>
2023-08-21 16:11:26 +0800

    feat(login)：新增mp4音频

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,15 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goLogin = () => {
+  router.push('/login')
+}
+</script>
 <template>
   <div>
     首页
   </div>
+  <!-- 去登录 -->
+  <!-- <router-link to="/login">去登录</router-link> -->
+  <van-button type="success" @click="goLogin">去登录</van-button>
 </template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,61 @@
-<script lang="setup">
-
-</script>
 <template>
-  <div>
-    登录
-    <van-button type="primary">主要按钮</van-button>
+  <div class="login">
+    <div class="video-container">
+      <video autoplay muted loop>
+        <source src="../../assets/images/login_bg.mp4" type="video/mp4">
+      </video>
+    </div>
+    <div class="fallback"></div>
+    <div class="btnClass">
+      登录
+    </div>
   </div>
 </template>
+
+<style lang="less" scoped>
+.login {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+.video-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+video {
+  width: 100%;
+  height: auto;
+  min-width: 100%;
+  min-height: 100%;
+}
+.btnClass{
+  position: absolute;
+  top: 30vh;
+  left: 0;
+  width: 100vw;
+  height: 80vh;
+  text-align: center;
+  background: #fff;
+  border-radius: 5px;
+  color: aqua;
+  cursor: pointer;
+  z-index: 999;
+  border-radius: 15px;
+}
+.fallback {
+  background: url('http://img.haihaina.cn/image/video_000/video_000.jpg') no-repeat;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
